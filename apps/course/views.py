22d42963c46ae0e389c0086fe0841f7fc88b6057
Eric Costa <ericosta.dev@gmail.com>
2022-08-08 21:38:49 -0300

    Overwriting generic views

--- apps/course/views.py
+++ apps/course/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Course, Avaliation
 from .serializers import CourseSerializer,AvaliationSerializer
@@ -17,7 +18,18 @@ class AvaliationsAPIView(generics.ListCreateAPIView):
     queryset = Avaliation.objects.all()
     serializer_class = AvaliationSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('course_pk'):
+            return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+        return self.queryset.all()
 
 class AvaliationAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliation.objects.all()
     serializer_class = AvaliationSerializer
+
+    def get_object(self):
+        if self.kwargs.get('course_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                    course_id=self.kwargs.get('course_pk'),
+                                    pk=self.kwargs.get('avaliation_pk'))
+        return get_object_or_404(self.get_queryset(),pk=self.kwargs.get('avaliation_pk'))
